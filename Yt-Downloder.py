bca80a47390afc74e7a6bdc29ba1d21048d3de83
H3LLO-H4CK3R <68962528+H3LLO-H4CK3R@users.noreply.github.com>
2020-10-12 22:47:59 +0530

    Add files via upload

---
+++ Yt-Downloder.py
@@ -0,0 +1,58 @@
+from pytube import YouTube
+import time
+import sys
+import os
+import youtube_dl
+print("\033[1;32;40m")
+os.system('clear')
+time.sleep(2)
+print (" YOUTUBE VIDEO  DOWNLODER  ")
+print ("########################################")
+time.sleep(3)
+print ( "_______________________________ " )
+print ( "|  Coded by H3LL0-H4CK3R       |" )
+print ( "|  Thanks for using my tool    |" )
+print ( "|______________________________|" )
+time.sleep(3)
+print("\033[1;33;40m ")
+print("###################################")
+print("\033[1;34;40m")
+time.sleep(3)
+print("PROGRAM STARTED")
+time.sleep(3)
+print("Please Wait")
+print("\033[1;31;40m")
+time.sleep(2.5)
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+spinner = spinning_cursor()
+for _ in range(50):
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    time.sleep(0.1)
+    sys.stdout.write('\b')
+time.sleep(3.5)
+print("\033[1;32;40m")
+time.sleep(3)
+print("###################################")
+print("\033[1;36;40m")
+time.sleep(3)
+print("DOWNLODE YOUTUBE VIDEOS WITH MY TOOL ")
+print("\033[1;31;40m")
+time.sleep(3)
+print("###################################")
+print("\033[1;32;40m")
+time.sleep(3)
+link = input("Enter Your Link Here :  ")
+print("\033[1;36;40m")
+print ("Please wait")
+time.sleep(2)
+print("Downloding...")
+YouTube(link).streams.first().download()
+print("\033[1;35;40m")
+print("Video downloaded successfully")
+time.sleep(3)
+print("\033[1;33;40m ")
+print("PROGRAM FINISHED")
